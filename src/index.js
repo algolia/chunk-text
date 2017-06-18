e49c3c487955c1a918d6ebb78105b3b8665683ac
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-06-18 17:15:11 +0200

    refactor: renamed "exploded" to more explicit "chunks"

--- src/index.js
+++ src/index.js
@@ -18,22 +18,22 @@ export default function(text, chunkSize) {
   assertIsValidText(text);
   assertIsValidChunkSize(chunkSize);
 
-  const exploded = [];
+  const chunks = [];
   while (true) { // eslint-disable-line
     if (text.length <= chunkSize) {
-      exploded.push(text);
+      chunks.push(text);
       break;
     }
     const splitAt = text.lastIndexOf(' ', chunkSize);
     if (splitAt === -1) {
       // No whitespace found, we need to truncate the word in that case.
-      exploded.push(text.substr(0, chunkSize));
+      chunks.push(text.substr(0, chunkSize));
       text = text.substr(chunkSize); // eslint-disable-line no-param-reassign
     } else {
-      exploded.push(text.substr(0, splitAt));
+      chunks.push(text.substr(0, splitAt));
       text = text.substr(splitAt + 1); // eslint-disable-line no-param-reassign
     }
   }
 
-  return exploded;
+  return chunks;
 }
