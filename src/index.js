7f21438219970a9cc7b8fbb601da78c5bd25ce08
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-06-18 16:12:40 +0200

    refactor: apply lint fixes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 export default function(text, size) {
   if (typeof text !== 'string') {
-    throw new TypeError('Text should be provided as first argument and be a string.');
+    throw new TypeError(
+      'Text should be provided as first argument and be a string.'
+    );
   }
 
   if (typeof size !== 'number' || size <= 0) {
-    throw new TypeError('Size should be provided as 2nd argument and be a number greater than zero.');
+    throw new TypeError(
+      'Size should be provided as 2nd argument and be a number greater than zero.'
+    );
   }
 
   const exploded = [];
@@ -13,7 +17,7 @@ export default function(text, size) {
       exploded.push(text);
       break;
     }
-    let splitAt = text.lastIndexOf(' ', size);
+    const splitAt = text.lastIndexOf(' ', size);
     if (splitAt === -1) {
       // No whitespace found, we need to truncate the word in that case.
       exploded.push(text.substr(0, size));
