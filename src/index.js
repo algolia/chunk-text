bca34005cae29e02ba01f916cd1a2473652e9d06
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-06-18 15:44:27 +0200

    chore: init

---
+++ src/index.js
@@ -0,0 +1,28 @@
+export default function(text, size) {
+  if (typeof text !== 'string') {
+    throw new TypeError('Text should be provided as first argument and be a string.');
+  }
+
+  if (typeof size !== 'number' || size <= 0) {
+    throw new TypeError('Size should be provided as 2nd argument and be a number greater than zero.');
+  }
+
+  const exploded = [];
+  while (true) {
+    if (text.length <= size) {
+      exploded.push(text);
+      break;
+    }
+    let splitAt = text.lastIndexOf(' ', size);
+    if (splitAt === -1) {
+      // No whitespace found, we need to truncate the word in that case.
+      exploded.push(text.substr(0, size));
+      text = text.substr(size);
+    } else {
+      exploded.push(text.substr(0, splitAt));
+      text = text.substr(splitAt + 1);
+    }
+  }
+
+  return exploded;
+}
